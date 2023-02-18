fa0c1d4e473cb059a5ba75fb84d8cc02e8f806d8
ElyMuniz <elymuzz@hotmail.com>
2023-02-18 11:45:32 -0300

    Criação do CRUD da rota de anuncios: interfaces, services, controllers e routes

---
+++ src/Services/Anouncement/listAnouncement.service.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+const listAnouncementService = async () =>{
+
+    const anouncement = await prisma.anouncement.findMany()
+    
+    return anouncement
+  }
+
+export default listAnouncementService
